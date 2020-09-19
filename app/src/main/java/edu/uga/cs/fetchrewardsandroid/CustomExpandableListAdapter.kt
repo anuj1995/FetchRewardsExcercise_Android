9e0cd56cb4a061f900460b2dc2b1fdc9e2ed9905
anuj panchmia <ap83035@uga.edu>
2020-09-19 18:58:52 -0400

    list view and its adapter, adjusted the gradle dependencies

---
+++ app/src/main/java/edu/uga/cs/fetchrewardsandroid/CustomExpandableListAdapter.kt
@@ -0,0 +1,85 @@
+package edu.uga.cs.fetchrewardsandroid
+
+import android.content.Context
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import edu.uga.cs.fetchrewardsexcercise.DataModels.HiringInfo
+
+class CustomExpandableListAdapter(var context:Context, var listTitles: List<Int>, val listDetails:
+    Map<Int, List<HiringInfo>>): BaseExpandableListAdapter() {
+    override fun getGroup(groupPosition: Int): Any {
+        return listTitles[groupPosition]
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(
+        groupPosition: Int,
+        isExpanded: Boolean,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+        var convertView = convertView
+        val listTitle = getGroup(groupPosition) as Int
+        if (convertView == null) {
+            val layoutInflater =
+            this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.list_group, null)
+        }
+        val listTitleTextView = convertView!!.findViewById<TextView>(R.id.listTitle)
+        listTitleTextView.setTypeface(null, Typeface.BOLD)
+        listTitleTextView.text = "List id:$listTitle"
+        return convertView
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return listDetails[listTitles[groupPosition]]?.size ?: 0
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        return listDetails[listTitles[groupPosition]]!![childPosition]
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildView(
+        groupPosition: Int,
+        childPosition: Int,
+        isLastChild: Boolean,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+        var convertView = convertView
+        val hiringInfo = getChild(groupPosition,childPosition) as HiringInfo
+        if(convertView == null){
+            val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = layoutInflater.inflate(R.layout.list_item,null)
+        }
+        val nameTextView = convertView!!.findViewById<TextView>(R.id.expandedListItem1)
+        nameTextView?.text = "Name: ${hiringInfo.name}"
+        val idTextView = convertView.findViewById<TextView>(R.id.expandedListItem2)
+        idTextView?.text = "Id: ${hiringInfo.id}"
+        return convertView
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun getGroupCount(): Int {
+        return listTitles.size
+    }
+
+}
--- app/src/main/java/edu/uga/cs/fetchrewardsandroid/MainActivity.kt
+++ app/src/main/java/edu/uga/cs/fetchrewardsandroid/MainActivity.kt
@@ -2,11 +2,28 @@ package edu.uga.cs.fetchrewardsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ExpandableListView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import edu.uga.cs.fetchrewardsexcercise.ViewModels.HiringInfoViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var expandableListView: ExpandableListView
+    lateinit var customExpandableListAdapter: CustomExpandableListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        expandableListView =  findViewById(R.id.expandableListView)
+        val viewModel = ViewModelProvider(this).get(HiringInfoViewModel::class.java)
+        viewModel.fetchData()
+        viewModel.getHiringInfoData().observe(this, Observer {
+            // update UI
+            customExpandableListAdapter = CustomExpandableListAdapter(this, it.keys.toList(), it)
+            expandableListView.setAdapter(customExpandableListAdapter)
+
+        })
     }
 }
